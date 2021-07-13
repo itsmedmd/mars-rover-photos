ea45d5ec5b09da5d641a10b411d9455a99476418
Deimantas Butėnas <itsmedmd1@gmail.com>
2021-07-13 17:59:40 +0300

    adding masonry-layout module for masonry gallery layout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["mars.nasa.gov"],
+    domains: ["mars.nasa.gov", "mars.jpl.nasa.gov"],
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import styles from "styles/home.module.scss";
+import imageStyles from "components/roverImage/rover-image.module.scss";
 import { Layout, RoverImage } from "components";
+//import Masonry from "masonry-layout";
 
 export const getStaticProps = async () => {
   const rovers = ["perseverance", "curiosity", "opportunity", "spirit"];
@@ -9,17 +12,20 @@ export const getStaticProps = async () => {
   let newestDate; // date of the most recent photo calculated from all rovers
 
   for (let rover of rovers) {
+    //`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.NASA_API_KEY}`
     // fetch rover photos and add it to the array
     const res = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.NASA_API_KEY}`
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?api_key=${process.env.NASA_API_KEY}&sol=1000`
     );
     const newData = await res.json();
-    data = data.concat(newData.latest_photos);
+    data = data.concat(newData.photos);
 
     // find if there is a more recent photo from the new set of data
-    const newDate = new Date(newData.latest_photos[0].earth_date);
-    if (!newestDate || newDate.getTime() > newestDate.getTime()) {
-      newestDate = newDate;
+    if (newData?.photos[0]?.earth_date) {
+      const newDate = new Date(newData.photos[0].earth_date);
+      if (!newestDate || newDate.getTime() > newestDate.getTime()) {
+        newestDate = newDate;
+      }
     }
   }
 
@@ -37,13 +43,13 @@ export const getStaticProps = async () => {
 };
 
 const Home = (props) => {
-  const { data: marsPhotos, newestDate } = props;
+  const { data, newestDate } = props;
 
-  const photosToRender = marsPhotos.map((photo) => {
+  const photosToRender = data.map((photo) => {
     const imageProps = {
       src: photo.img_src,
       layout: "fill",
-      quality: "30",
+      quality: "90",
       alt: `${photo.rover.name} Mars rover image with ${photo.camera.full_name} on ${photo.earth_date}`,
     };
 
@@ -56,20 +62,33 @@ const Home = (props) => {
     );
   });
 
+  useEffect(() => {
+    const initMasonry = async () => {
+      if (typeof window !== "undefined") {
+        const { default: Masonry } = await import("masonry-layout");
+
+        const myMasonry = new Masonry("." + styles.gallery, {
+          itemSelector: "." + imageStyles.observer,
+          columnWidth: 500,
+        });
+      }
+    };
+
+    initMasonry();
+  }, []);
+
   return (
     <Layout>
       <Head>
         <title>Deimantas Butėnas - Mars Rover Photos</title>
       </Head>
-      <div className={styles.gallery}>
-        <h1 className={styles.text}>
-          Displaying photos of the most recent Sol (day on Mars).
-        </h1>
-        <h2 className={styles.text}>
-          Most recent image received at {newestDate}.
-        </h2>
-        {photosToRender}
-      </div>
+      <h1 className={styles.text}>
+        Displaying photos of the most recent Sol (day on Mars).
+      </h1>
+      <h2 className={styles.text}>
+        Most recent image received at {newestDate}.
+      </h2>
+      <div className={styles.gallery}>{photosToRender}</div>
     </Layout>
   );
 };
