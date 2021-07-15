0d29d7c0567370559a46ac2c359c15feb0b35173
Deimantas Butėnas <itsmedmd1@gmail.com>
2021-07-15 21:24:12 +0300

    adding code to create data files to be reused instead of making the same API requests

--- pages/[page].js
+++ pages/[page].js
@@ -1,49 +1,22 @@
 import styles from "styles/home.module.scss";
 import { Layout, RoverImage } from "components";
 
-/*export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory)
-
-  // Returns an array that looks like this:
-  // [
-  //   {
-  //     params: {
-  //       id: 'ssg-ssr'
-  //     }
-  //   },
-  //   {
-  //     params: {
-  //       id: 'pre-rendering'
-  //     }
-  //   }
-  // ]
-  return fileNames.map(fileName => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, '')
-      }
-    }
-  })
-} */
-
 export async function getStaticPaths() {
-  const rovers = ["perseverance", "curiosity", "opportunity", "spirit"];
-  let data = []; // latest photos from all rovers
+  const fs = require("fs");
+  const fileName = "./data/pages-count.json";
+  let pageCount;
 
-  for (let rover of rovers) {
-    //`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.NASA_API_KEY}`
-    // fetch rover photos and add it to the array
-    const res = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?api_key=${process.env.NASA_API_KEY}&sol=1000`
-    );
-    const newData = await res.json();
-    data = data.concat(newData.photos);
+  try {
+    const rawData = fs.readFileSync(fileName);
+    const data = JSON.parse(rawData);
+    pageCount = data.pageCount;
+    console.log(`success reading from file ${fileName}`);
+  } catch (err) {
+    console.error(`error reading from file ${fileName}`, err);
   }
 
-  const pagesCount = data.length / 10;
   const paths = [];
-
-  for (let i = 1; i <= pagesCount; i++) {
+  for (let i = 1; i <= pageCount; i++) {
     paths.push({
       params: {
         page: `page-${i * 10}`,
@@ -51,7 +24,7 @@ export async function getStaticPaths() {
     });
   }
 
-  console.log("all paths:", paths);
+  //console.log("all paths:", paths);
 
   return {
     paths,
@@ -59,37 +32,17 @@ export async function getStaticPaths() {
   };
 }
 
-/*
-export function getPostData(id) {
-  const fullPath = path.join(postsDirectory, `${id}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents)
-
-  // Combine the data with the id
-  return {
-    id,
-    ...matterResult.data
-  }
-} */
-
 export async function getStaticProps({ params }) {
-  const rovers = ["perseverance", "curiosity", "opportunity", "spirit"];
+  const fs = require("fs");
+  const fileName = "./data/images-data.json";
   let data = []; // latest photos from all rovers
 
-  for (let rover of rovers) {
-    //`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.NASA_API_KEY}`
-    // fetch rover photos and add it to the array
-    const res = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?api_key=${process.env.NASA_API_KEY}&sol=1000`
-    );
-    const newData = await res.json();
-    data = data.concat(newData.photos);
-  }
-
-  if (!data) {
-    return { notFound: true };
+  try {
+    const rawData = fs.readFileSync(fileName);
+    data = JSON.parse(rawData);
+    console.log(`success reading from file ${fileName}`);
+  } catch (err) {
+    console.error(`error reading from file ${fileName}`, err);
   }
 
   let pageNumber = params.page.split("-");
@@ -107,7 +60,7 @@ const Page = ({ data }) => {
     const imageProps = {
       src: photo.img_src,
       layout: "fill",
-      quality: "35",
+      quality: "30",
       alt: `${photo.rover.name} Mars rover image with ${photo.camera.full_name} on ${photo.earth_date}`,
     };
 
@@ -116,6 +69,7 @@ const Page = ({ data }) => {
     );
   });
 
+  console.log("rendering a page.");
   return (
     <Layout>
       <div className={styles.gallery}>{photosToRender}</div>
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,28 @@ import { Layout, RoverImage } from "components";
 import imagesLoaded from "imagesloaded";
 
 export const getStaticProps = async () => {
+  const fs = require("fs");
   const rovers = ["perseverance", "curiosity", "opportunity", "spirit"];
   let data = []; // latest photos from all rovers
   let newestDate; // date of the most recent photo calculated from all rovers
+
+  const writeToFile = (fileName, dataToWrite) => {
+    // delete file before write because keeping the file causes
+    // the content to be appended instead of overwritten
+    fs.unlinkSync(fileName);
+    fs.writeFile(
+      fileName,
+      JSON.stringify(dataToWrite),
+      { flag: "a+" },
+      (err) => {
+        if (err) {
+          console.error(`error writing to file ${fileName}`, err);
+          return;
+        }
+        console.log(`success writing to file ${fileName}`);
+      }
+    );
+  };
 
   for (let rover of rovers) {
     //`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.NASA_API_KEY}`
@@ -26,6 +45,13 @@ export const getStaticProps = async () => {
         newestDate = newDate;
       }
     }
+
+    // logging NASA API x-ratelimit-limit and x-ratelimit-remaining headers
+    for (let pair of res.headers.entries()) {
+      if (pair[0].includes("ratelimit")) {
+        console.log(pair[0] + ": " + pair[1]);
+      }
+    }
   }
 
   if (!data) {
@@ -34,6 +60,9 @@ export const getStaticProps = async () => {
 
   // format the date to have format of "YYYY-MM-DD"
   newestDate = newestDate.toISOString().split("T")[0];
+
+  writeToFile("./data/images-data.json", data);
+  writeToFile("./data/pages-count.json", { pageCount: data.length / 10 });
 
   data = data.slice(0, 10);
 
@@ -51,7 +80,7 @@ const Home = (props) => {
     const imageProps = {
       src: photo.img_src,
       layout: "fill",
-      quality: "35",
+      quality: "30",
       alt: `${photo.rover.name} Mars rover image with ${photo.camera.full_name} on ${photo.earth_date}`,
     };
 
@@ -97,6 +126,7 @@ const Home = (props) => {
     initMasonry();
   }, []);
 
+  console.log("rendering index.");
   return (
     <Layout>
       <Head>
