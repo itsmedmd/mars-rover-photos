fdff9d55090e3026b8dcbe5e2acc6d6b6e808f2f
Deimantas Butėnas <itsmedmd1@gmail.com>
2021-08-08 21:45:57 +0300

    adding active page style for navigation

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,14 +1,41 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "./navigation.module.scss";
 
 export const Navigation = () => {
+  const [isHomeCurrentPage, setIsHomeCurrentPage] = useState(true);
+
+  useEffect(() => {
+    if (window.location.pathname === "/rovers") {
+      setIsHomeCurrentPage(false);
+    } else if (window.location.pathname === "/") {
+      setIsHomeCurrentPage(true);
+    } else {
+      setIsHomeCurrentPage(null);
+    }
+  }, []);
+
   return (
     <nav className={styles["navigation"]}>
       <Link href="/">
-        <a className={styles["navigation__item"]}>Photos</a>
+        <a
+          className={`${styles["navigation__item"]} ${
+            isHomeCurrentPage ? styles["navigation__item--current"] : ""
+          }`}
+        >
+          Photos
+        </a>
       </Link>
       <Link href="/rovers">
-        <a className={styles["navigation__item"]}>Rovers</a>
+        <a
+          className={`${styles["navigation__item"]} ${
+            isHomeCurrentPage === false
+              ? styles["navigation__item--current"]
+              : ""
+          }`}
+        >
+          Rovers
+        </a>
       </Link>
     </nav>
   );
