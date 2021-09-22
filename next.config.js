fe0faa4d4494b79b456a74e0a75e793fa07fcc9f
Deimantas Butėnas <itsmedmd1@gmail.com>
2021-09-22 18:39:44 +0300

    updating image minimumCacheTTL

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["mars.nasa.gov", "mars.jpl.nasa.gov"],
-    minimumCacheTTL: 15768000,
+    minimumCacheTTL: 31536000,
   },
 };
